2da7ba8a09c9efd43961a56e8ada171492c60920
krystian-laubach-jr <krystian.laubach.jr@gmail.com>
2026-03-16 09:13:12 +0100

    Rozdzielono BlockDisplay z TetrisNext do osobnego komponentu

--- src/TetrisNext.jsx
+++ src/TetrisNext.jsx
@@ -1,4 +1,6 @@
 import './TetrisNext.css';
+import BlockDisplay from './BlockDisplay';
+
 function TetrisNext({nextPiece, nextColor, onNextClick}) {
 
   return (
@@ -6,77 +8,7 @@ function TetrisNext({nextPiece, nextColor, onNextClick}) {
       <div className='border'>
         <div id='tetrisNextContainer'>
           <h1>Next up:</h1>
-
-          <div id="nextGrid">
-
-            <div className='nextGridRow'>
-              <div className='nextGridCell'>
-              </div>
-
-              <div className='nextGridCell'>
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'line') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-              </div>
-            </div>
-
-            <div className='nextGridRow'>
-              <div className='nextGridCell'>
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'l') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'line' || nextPiece == 'rl') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-              </div>
-            </div>
-
-            <div className='nextGridRow'>
-              <div className='nextGridCell'>
-                {(nextPiece == 'z') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'z' || nextPiece == 'square' || nextPiece == 'l') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-              
-              <div className='nextGridCell'>
-                {(nextPiece == 'line' || nextPiece == 'square' || nextPiece == 't' || nextPiece == 'rz' || nextPiece == 'rl') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'rz') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-            </div>
-
-            <div className='nextGridRow'>
-              <div className='nextGridCell'>
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'z' || nextPiece == 'square' || nextPiece == 'l' || nextPiece == 't' || nextPiece == 'rz' || nextPiece == 'rl') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 'line' || nextPiece == 'z' || nextPiece == 'square' || nextPiece == 'l' || nextPiece == 't' || nextPiece == 'rz' || nextPiece == 'rl') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-
-              <div className='nextGridCell'>
-                {(nextPiece == 't') && <div className={`brick ${nextColor}`}></div>}
-              </div>
-            </div>
-
-          </div>
-          
+          <BlockDisplay piece={nextPiece} color={nextColor}/>
           <p>{nextPiece}</p>
           <button onClick={() => onNextClick()}>Next</button>
         </div>
