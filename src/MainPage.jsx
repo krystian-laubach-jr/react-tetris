e39d39ddfd3bd3e5c025d3a1287058eda4704100
Czadiners <mat3uszkuleta@gmail.com>
2026-04-23 11:07:38 +0200

    gui git chyba
    jem kabanosy

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -17,41 +17,24 @@ export default function MainPage() {
         <>
           <div className="guzikiborder">
             <div className="button-column">
-              <div className="border">
                 <button 
                   className="btn btn1"
                   onClick={() => setShowGame(true)}
                 >
                   Graj
                 </button>
-              </div>
 
-              <div className="border">
                 <button className="btn btn1">Wyniki</button>
-              </div>
-              <div className="border">
-                <button className="btn btn1">Styl</button>
-              </div>
-             <div className="border">
+                <button className="btn btn1">Sterowanie</button>
                 <a 
                   href="https://github.com/krystian-laubach-jr/react-tetris.git" 
                   target="_blank" 
                   rel="noopener noreferrer">
                   <button className="btn btn1">Credits</button>
                 </a>
-              </div>
             </div>
           </div>
 
-          <div className="tabliczka">
-            <div className="border">
-              <div className="side-panel">
-                <h2>Panel</h2>
-                <p>Lorem ipsum dolor sit amet...</p>
-                <p>Ut enim ad minim veniam...</p>
-              </div>
-            </div>
-          </div>
         </>
       )} 
     </div>
